79244dfb0549acc92dcc991e0a045067adeb8c5f
BadTripp <renato.turetta00@gmail.com>
2022-04-13 12:28:05 +0200

    add localstorage save

--- src/app/gender/gender.component.ts
+++ src/app/gender/gender.component.ts
@@ -9,15 +9,21 @@ import { GenderService } from "./gender.service";
 export class GenderComponent{
     titolo:string ="Trova il genere";
     nome:string=""
-    scheda:any = {};
+    scheda:any = {}
+    arr :any[]=[];
     error:any;
-    constructor(private gs:GenderService){}
-
+    
+    constructor(private gs:GenderService){
+        console.log(this.arr)
+        this.arr=this.gs.loadData()
+    }
+    
     trovaGenere(){
         this.gs.getData(this.nome).subscribe({
             next:(dato:any)=>{
                 this.scheda=dato;
-                console.log(this.scheda)
+                this.arr.push(this.scheda)
+                this.gs.saveData(this.arr);
             },
             error:(err:any)=>{
                 this.error=err;
--- src/app/gender/gender.service.ts
+++ src/app/gender/gender.service.ts
@@ -8,6 +8,21 @@ export class GenderService{
     
     constructor(private http:HttpClient){}
 
+    saveData(names:any){
+        localStorage.setItem("names", JSON.stringify(names));
+    }
+    loadData(){
+        // JSON.parse(local)
+        let scheda:any = [{
+            name:"",
+            gender:"",
+            probability:"",
+            count:""
+        }]
+        let local=localStorage.getItem("names")
+        return (local)?JSON.parse(local):scheda;
+    }
+
     getData(nome:string):Observable<any>{
         let url="https://api.genderize.io?name="+nome
         return this.http.get<any>(url);
